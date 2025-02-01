8c48c3b8941a7e0eb9763421b4f659d026ce6900
Rajesh Rathod <96156957+rajeshrathod972@users.noreply.github.com>
2025-02-01 19:09:13 +0530

    D

---
+++ src/test/java/com/startDevops/StartDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startDevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
